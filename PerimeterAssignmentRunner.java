a153b98537e9de9766f5485bdac5213f4b4f2d8c
margacampos <margacampos@hotmail.es>
2024-01-24 22:09:26 +0000

    Perimeter Assignment: Part Two

--- PerimeterAssignmentRunner.java
+++ PerimeterAssignmentRunner.java
@@ -25,9 +25,37 @@ public class PerimeterAssignmentRunner {
         return avgLength;
     }
 
+    public double getLargestSide(Shape s) {
+        double lgstSide = 0;
+
+        Point prevPt = s.getLastPoint();
+
+        for (Point currPt : s.getPoints()) {
+            double currDist = prevPt.getDistance(currPt);
+            if (currDist > lgstSide) {
+                lgstSide = currDist;
+            }
+            prevPt = currPt;
+        }
+
+        return lgstSide;
+    }
+
+    public double getLargestX(Shape s) {
+        double lgstX = s.getLastPoint().getX();
+        for (Point currPt : s.getPoints()) {
+            if (lgstX < currPt.getX()) {
+                lgstX = currPt.getX();
+            }
+        }
+        return lgstX;
+    }
+
 }
 
 public void testPerimeter (Shape s) {
     System.out.println(getNumPoints(s));
     System.out.println(getAverageLength(s));
+    System.out.println(getLargestSide(s));
+    System.out.println(getLargestX(s));
 }
